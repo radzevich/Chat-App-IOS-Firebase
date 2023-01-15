d4b83aaa9b61ff86bc0e70c53520869274be48c6
Aliaksei Radzevich <aliaksei.radzevich@gmail.com>
2023-01-15 22:28:02 +0100

    Login + Register stub. Choosing a profile photo

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  ChatApp
-//
-//  Created by Aliaksei Radzevich on 08.01.2023.
-//
-
 import UIKit
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        if !isLoggedIn {
+            let loginController = LoginViewController()
+            let navigationController = UINavigationController(rootViewController: loginController)
+            navigationController.modalPresentationStyle = .fullScreen
 
+            present(navigationController, animated: true)
+        }
+    }
 }
 
